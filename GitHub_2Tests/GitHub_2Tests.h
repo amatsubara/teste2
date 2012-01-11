feb205f3fdd7e7e59ecb2f99a2a303755982e17a
Adriana Matsubara <adriana.matsubara@mowa.com.br>
2012-01-11 15:03:33 -0200

    Initial Commit

---
+++ GitHub_2Tests/GitHub_2Tests.h
@@ -0,0 +1,13 @@
+//
+//  GitHub_2Tests.h
+//  GitHub_2Tests
+//
+//  Created by Adriana Matsubara on 11/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GitHub_2Tests : SenTestCase
+
+@end
---
+++ GitHub_2Tests/GitHub_2Tests.m
@@ -0,0 +1,32 @@
+//
+//  GitHub_2Tests.m
+//  GitHub_2Tests
+//
+//  Created by Adriana Matsubara on 11/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GitHub_2Tests.h"
+
+@implementation GitHub_2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GitHub_2Tests");
+}
+
+@end
